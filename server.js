69cdb82a5d3bb099f4667688fb706c27c4131a23
hainga2505 <nguyenhai07sh@gmail.com>
2022-12-18 19:31:42 +0800

    edit file server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "https://frontend-reactjs-xi.vercel.app"
+  origin: "https://test-frontend-seven.vercel.app/",
 };
 
 app.use(cors(corsOptions));
@@ -19,12 +19,12 @@ const db = require("./app/models");
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to the database!");
   })
-  .catch(err => {
+  .catch((err) => {
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
