7b000003e597225bb8e84139f4b5a7eb9f676e16
hainga2505 <nguyenhai07sh@gmail.com>
2022-12-18 19:36:44 +0800

    edit server file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "https://test-frontend-seven.vercel.app/",
+  origin: "https://test-frontend-seven.vercel.app",
 };
 
 app.use(cors(corsOptions));
